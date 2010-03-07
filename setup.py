f456195a1679e0b47babc02a2a6b5b86cccc940a
Ilshad Khabibullin <astoon.net@gmail.com>
2010-03-07 20:34:34 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='ice.control',
+      version='0.1.0',
+      author='Ilshad Khabibullin',
+      author_email='astoon.net at gmail.com',
+      url='',
+      description='',
+      long_description='',
+      license='GPL v.3',
+      classifiers=[],
+      packages=find_packages('src'),
+      namespace_packages=['ice',],
+      package_dir={'':'src'},
+      install_requires=['setuptools',
+                        'z3c.testsetup',
+                        'z3c.layer.pagelet',
+                        'z3c.template']
+      include_package_data=True,
+      zip_safe=False)
