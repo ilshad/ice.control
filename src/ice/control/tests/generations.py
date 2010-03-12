140c6727cb7a68147d7ffa7b875629b52633917d
Ilshad Khabibullin <astoon.net@gmail.com>
2010-03-13 03:53:34 +0500

    database schema manager testing moved

---
+++ src/ice/control/tests/generations.py
@@ -0,0 +1,24 @@
+### -*- coding: utf-8 -*- ####################################################
+#
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation, either version 3 of the License.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from zope.app.generations.generations import SchemaManager
+
+pkg = 'ice.control.tests'
+
+schemaManager = SchemaManager(minimum_generation=0,
+                              generation=1,
+                              package_name=pkg)
+
+
