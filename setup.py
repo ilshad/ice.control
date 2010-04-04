fefe011fc9c412aa8906b793637102c95dafde5d
Ilshad Khabibullin <astoon.net@gmail.com>
2010-04-04 07:00:41 +0600

    setup info

--- setup.py
+++ setup.py
@@ -12,11 +12,20 @@ setup(name='ice.control',
       url='http://launchpad.net/ice.control',
       description='System Administration and Site Management for BlueBream',
       long_description = (read('src/ice/control/doc/README')) +\
-          '\n\n' + read('src/ice/control/doc/HACKME'),
-          '\n\n' + read('src/ice/control/doc/BUGS'),
+          '\n\n' + read('src/ice/control/doc/HACKME') +\
+          '\n\n' + read('src/ice/control/doc/BUGS') +\
           '\n\n' + read('src/ice/control/doc/CHANGES'),
       license='GPL v.3',
-      classifiers=[],
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Framework :: Zope3'],
       packages=find_packages('src'),
       namespace_packages=['ice',],
       package_dir={'':'src'},
